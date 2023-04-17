9c425a296756ec134d072a48398a317a5bd63393
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2023-04-17 23:29:06 +0800

    d

--- Pages/comment.php
+++ Pages/comment.php
@@ -35,7 +35,7 @@ $comment_count = count($rowsComment);
         <?php include("../Includes/header1.inc.php");?>
         
         <main class="main">
-        	<div class="page-header text-center" style="background-image: url('../img/Aquarium.jpg')">
+        	<div class="page-header text-center" style="background-image: url('../img/aq1.jpg')">
         		<div class="container">
         			<h1 class="page-title" style="color:#fff;font-weight:700">View More Details<span style="color:#fff;">Single Post</span></h1>
         		</div><!-- End .container -->
