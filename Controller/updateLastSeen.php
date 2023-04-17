d2344fbbe32d5fe35f4c56a3a8626ac68ffe44a0
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2023-04-17 20:03:49 +0800

    a

---
+++ Controller/updateLastSeen.php
@@ -0,0 +1,23 @@
+<?php  
+
+session_start();
+
+# check if the user is logged in
+if (isset($_SESSION['id'])) {
+	
+	# database connection file
+	include '../Model/dbPDO.php';
+
+	# get the logged in user's username from SESSION
+	$id = $_SESSION['id'];
+
+	$sql = "UPDATE user_details
+	        SET last_seen = NOW() 
+	        WHERE user_id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$id]);
+
+}else {
+	header("Location: ../Includes/logout.php");
+	exit;
+}
--- Pages/index.php
+++ Pages/index.php
@@ -177,6 +177,9 @@ session_start();
 
                   <a href="../Pages/breedersBlog.php">Breeders Blog</a>
                 </li>
+                <li>
+                    <a href="../Pages/chat.php">CHAT</a>
+                </li>
 
 
               </ul>
