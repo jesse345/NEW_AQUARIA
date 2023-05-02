0534a8c16580e343cc77750316c878ca6a1237c2
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2023-05-02 12:30:34 +0800

    a

--- Pages/chat.php
+++ Pages/chat.php
@@ -36,7 +36,7 @@ if(isset($_SESSION['id'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    
+  
     
     <title>CHAT</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -63,6 +63,7 @@ if(isset($_SESSION['id'])){
     </style>
 </head>
 <body>
+    
     <section>
         <div class="container py-4">
              <nav aria-label="breadcrumb" class="breadcrumb-nav">
